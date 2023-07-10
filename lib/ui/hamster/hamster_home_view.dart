c93e66e2decc4dadc199c0839da71870ff0159e5
Leonamin <yism810@gmail.com>
2023-07-10 14:06:48 +0900

    # ADD - 페이지 큐브 전환

--- lib/ui/hamster/hamster_home_view.dart
+++ lib/ui/hamster/hamster_home_view.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:montreuil_shrew_mer/constant/asset_constant.dart';
+import 'package:montreuil_shrew_mer/ui/component/cube_route_builder.dart';
+import 'package:montreuil_shrew_mer/ui/hamster/shrew_home_view.dart';
 
 class HamsterHomeView extends StatelessWidget {
   const HamsterHomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () => onTapMain(context),
+        child: const Image(
+          image: AssetImage(AssetConstant.hamsterHomeImage),
+          fit: BoxFit.fitHeight,
+        ),
+      ),
+    );
   }
+
+  void onTapMain(BuildContext context) =>
+      Navigator.of(context).pushAndRemoveUntil(
+        CubePageRoute(
+          const HamsterHomeView(),
+          const ShrewHomeView(),
+          // duration: Duration(seconds: 2),
+        ),
+        (route) {
+          return false;
+        },
+      );
 }
--- lib/ui/hamster/shrew_home_view.dart
+++ lib/ui/hamster/shrew_home_view.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:montreuil_shrew_mer/constant/asset_constant.dart';
+import 'package:montreuil_shrew_mer/ui/component/cube_route_builder.dart';
+import 'package:montreuil_shrew_mer/ui/hamster/hamster_home_view.dart';
 
 class ShrewHomeView extends StatelessWidget {
   const ShrewHomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () => onTapMain(context),
+        child: const Image(
+          image: AssetImage(AssetConstant.shrewHomeImage),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
   }
+
+  void onTapMain(BuildContext context) =>
+      Navigator.of(context).pushAndRemoveUntil(
+        CubePageRoute(
+          const ShrewHomeView(),
+          const HamsterHomeView(),
+        ),
+        (route) {
+          return false;
+        },
+      );
 }
